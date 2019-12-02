641f088fe670090492425f892a6634819f17905f
Aleksandar Nikolaev Dinkov <and@milestone.dk>
2019-12-02 18:09:17 +0200

    some CheckFinished tests

--- tests/queue.checkFinished.test.js
+++ tests/queue.checkFinished.test.js
@@ -4,15 +4,15 @@ function createQueue(config = {}) {
     var queue = null;
 
     config.maxTaskAge = config.maxTaskAge || 1000, // 1 sec
-        config.maxRetries = config.maxRetries || 5,
-        config.maxParallelTasks = config.maxParallelTasks || 2,
-        config.refreshPeriod = config.refreshPeriod || 100,
-        config.persistPeriod = config.persistPeriod || 9999999,
-        config.retryWaitPeriod = config.retryWaitPeriod || 150,
-        config.checkDoneWaitPeriod = config.checkDoneWaitPeriod || 40,
-        config.checkFinishedTimeout = config.checkFinishedTimeout || 100,
-        config.logger = config.logger || (str => { })
-    config.startTask = config.startTask || ((i, t) => { 
+    config.maxRetries = config.maxRetries || 5,
+    config.maxParallelTasks = config.maxParallelTasks || 2,
+    config.refreshPeriod = config.refreshPeriod || 50,
+    config.persistPeriod = config.persistPeriod || 9999999,
+    config.retryWaitPeriod = config.retryWaitPeriod || 150,
+    config.checkDoneWaitPeriod = config.checkDoneWaitPeriod || 200,
+    config.checkFinishedTimeout = config.checkFinishedTimeout || 1000,
+    config.logger = config.logger || (str => {})
+    config.startTask = config.startTask || ((i, t) => {
         queue.startsList.push(t)
     })
     if (!config.checkFinishedAsync && !config.checkFinished)
@@ -26,13 +26,113 @@ function createQueue(config = {}) {
     return queue
 }
 
-// test("CheckFinishedAsync has priority over CheckFinished", done => { throw new Error("Not Implemented") });
-// test("CheckFinished delay", done => { throw new Error("Not Implemented") });
-// test("CheckFinishedAsync delay", done => { throw new Error("Not Implemented") });
-// test("CheckFinishedAsync is indeed async - x10", done => { throw new Error("Not Implemented") });
-// test("CheckFinishedAsync Fail state", done => { throw new Error("Not Implemented") });
-// test("CheckFinished Fail state", done => { throw new Error("Not Implemented") });
-// test("CheckFinishedAsync unknown state", done => { throw new Error("Not Implemented") });
-// test("CheckFinished unknown state", done => { throw new Error("Not Implemented") });
-// test("CheckFinishedAsync timeout", done => { throw new Error("Not Implemented") });
-// test("CheckFinished timeout", done => { throw new Error("Not Implemented") });
+test("CheckFinishedAsync has priority over CheckFinished", done => {
+    var calledMethods = []
+    var queue = createQueue({
+        checkFinished: (i, t) => {
+            calledMethods.push("checkFinished")
+            return true
+        },
+        checkFinishedAsync: (i, t, c) => {
+            calledMethods.push("checkFinishedAsync")
+            c(true)
+        }
+    })
+    queue.start()
+    queue.push("1")
+    setTimeout(() => {
+        expect(calledMethods.length).toBe(1)
+        expect(calledMethods[0]).toBe("checkFinishedAsync")
+        queue.stop()
+        done()
+    }, 300);
+});
+
+test("CheckFinished delay", done => {
+    var flag = true;
+    var queue = true
+    queue = createQueue({
+        checkFinished: ((i, t) => {
+            queue.checksList.push(t)
+            if(flag) {
+                flag = false
+                return "ErroRr"
+            }
+            return true
+        })
+    })
+    queue.start()
+    queue.push("1")
+    setTimeout(() => {
+        expect(queue.startsList.length).toBe(1)
+        expect(queue.startsList[0]).toBe("1")
+        setTimeout(() => {
+            expect(queue.checksList.length).toBe(2)
+            expect(queue.startsList[1]).toBe("1")
+            setTimeout(() => {
+                expect(queue.checksList.length).toBe(2)
+                queue.stop()
+                done()
+            }, 500);
+        }, 260);
+    }, 260);
+});
+
+test("CheckFinishedAsync delay", done => {
+    var flag = true;
+    var queue = true
+    queue = createQueue({
+        checkFinishedAsync: ((i, t, c) => {
+            queue.checksList.push(t)
+            if(flag) {
+                flag = false
+                c("ErroRr")
+                return
+            }
+            c(true)
+        })
+    })
+    queue.start()
+    queue.push("1")
+    setTimeout(() => {
+        expect(queue.startsList.length).toBe(1)
+        expect(queue.startsList[0]).toBe("1")
+        setTimeout(() => {
+            expect(queue.checksList.length).toBe(2)
+            expect(queue.startsList[1]).toBe("1")
+            setTimeout(() => {
+                expect(queue.checksList.length).toBe(2)
+                queue.stop()
+                done()
+            }, 500);
+        }, 260);
+    }, 260);
+});
+
+test("CheckFinishedAsync is indeed async - x10", done => {
+
+});
+
+test("CheckFinishedAsync Fail state", done => {
+
+});
+
+test("CheckFinished Fail state", done => {
+
+});
+
+test("CheckFinishedAsync unknown state", done => {
+
+});
+
+test("CheckFinished unknown state", done => {
+
+});
+
+test("CheckFinishedAsync timeout", done => {
+
+});
+
+test("CheckFinished timeout", done => {
+
+});
